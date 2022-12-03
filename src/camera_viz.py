e92eaa6bcf37120ca56a8c606bf3330af68c5790
KueiKao <r08921109@ntu.edu.tw>
2022-12-03 15:45:20 +0800

    script for flying tellos to record video image for VIO testing

---
+++ src/camera_viz.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import sys
+import yaml
+import rospy
+import numpy as np
+import cv2
+from cv2 import aruco
+from cv_bridge import CvBridge
+from sensor_msgs.msg import Image
+from geometry_msgs.msg import Point
+
+class MarkerDetect():
+    def __init__(self, tello_ns):      
+        # [ cv Bridge ]
+        self.br = CvBridge()
+        
+        # [ ROS publisher subscriber ]
+        self.sub_image = rospy.Subscriber("/%s/camera/image_raw" % tello_ns, Image, self.cb_image, queue_size = 1)
+
+    def cb_image(self, img_raw):
+        ## cvBridge
+        frame = self.br.imgmsg_to_cv2(img_raw)
+
+        ## gray
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        cv2.imshow("frame",frame)
+        key = cv2.waitKey(1)
+
+def main():
+    rospy.init_node('camera_viz', anonymous=True)
+    MarkerDetect(tello_ns)
+    rospy.spin()
+
+
+if __name__ == '__main__':
+    tello_ns = sys.argv[1]
+    main()
--- src/talker.py
+++ src/talker.py
@@ -8,10 +8,12 @@ from std_msgs.msg import Empty
 from geometry_msgs.msg import Point
 from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
+from tf.transformations import quaternion_matrix
 
 class Talker():
     def __init__(self):      
-        self.odom = np.array([0,0,0])
+        self.odom_pos = np.array([0,0,0])
+        self.odom_orien = np.array([[0,0,0], [0,0,0], [0,0,0]])
         self.marker = np.array([0,0,0])
 
         self.pub_motor_on = rospy.Publisher('manual_takeoff', Empty, queue_size=1)
@@ -21,15 +23,98 @@ class Talker():
         self.sub_odom = rospy.Subscriber('odom', Odometry, self.cb_odom, queue_size = 1)
         self.sub_marker = rospy.Subscriber('marker', Point, self.cb_marker, queue_size = 1)
 
-        rospy.sleep(2.0) # warm up for publishing
+        rospy.sleep(6.0) # warm up for publishing
 
         # experiment
-        self.case_motor_on_and_land()
+        self.case_flyUp_rotate_Land()
 
-    def case_motor_on_and_land(self):
+    def case_motorOn_and_land(self):
         self.util_motor_on()
-        rospy.sleep(2.0)
+        self.util_wait(2.0)
         self.util_land()
+
+    def case_motorOn_flyUp_Land(self):
+        self.util_motor_on()
+        self.util_wait(2.0)
+        self.util_flyup(0.5)
+        self.util_wait(5)
+        self.util_hover()
+        self.util_wait(0.5)
+        self.util_land()
+
+    def case_flyUp_rotate_Land(self):
+        self.util_motor_on()
+        self.util_wait(2.0)
+
+        # fly up
+        self.util_flyup(1)
+        self.util_wait(1.5)
+
+        self.util_hover()
+        self.util_wait(0.5)
+
+        # back ward
+        self.util_cmd(0,-1,0,0)
+        self.util_wait(1.0)
+
+        self.util_hover()
+        self.util_wait(0.5)
+
+        # forward
+        self.util_cmd(0,1,0,0)
+        self.util_wait(1.0)
+
+        self.util_hover()
+        self.util_wait(0.5)
+
+        # rotate
+        self.util_cmd(0,0,0,1)
+        self.util_wait(0.6)
+
+        self.util_hover()
+        self.util_wait(1.2)
+
+        # rotate
+        self.util_cmd(0,0,0,-1)
+        self.util_wait(0.6)
+
+        self.util_hover()
+        self.util_wait(0.5)
+
+        self.util_land()
+
+    def case_alignXY_constZ(self):
+        # self.util_motor_on()
+        # self.util_wait(2.0)
+        # self.util_land()
+        # print("case_alignXY_trackZ")
+        self.control_alignXY_constZ(5, 5)
+    
+    def control_alignXY_constZ(self, duration, z):
+        Kp_x = 1
+        Kp_y = 1
+        Kp_z = 1
+
+        starttime = time.time()
+        rate = rospy.Rate(10) 
+
+        while not rospy.is_shutdown():
+
+            curret = time.time()
+            sofar = curret - starttime
+
+            if (sofar < duration) :
+                rate.sleep()
+            else:
+                break
+
+    def util_cmd(self, x, y, z, yaw):
+        msg = Twist()
+        msg.linear.x = x
+        msg.linear.y = y
+        msg.linear.z = z
+        msg.angular.z = yaw
+        self.pub_cmd_vel.publish(msg)
 
     def util_wait(self, duration):
         rospy.sleep(duration)
@@ -38,28 +123,45 @@ class Talker():
         self.pub_motor_on.publish()
 
     def util_hover(self):
-        msg = Twist()
-        self.pub_cmd_vel.publish(msg)
+        self.util_cmd(0,0,0,0)
 
     def util_land(self):
         self.pub_land.publish()
 
-    def util_flyup(self, thrust, duration):
-        msg = Twist()
-        msg.linear.z = thrust
-        self.pub_cmd_vel.publish(msg)
-        rospy.sleep(duration)
-
-        msg = Twist()
-        self.pub_cmd_vel.publish(msg)
+    def util_flyup(self, thrust):
+        self.util_cmd(0,0,thrust,0)
 
     def cb_odom(self, odom):
         pos = odom.pose.pose.position
-        self.odom = np.array([pos.x, pos.y, pos.z])
+        orien = odom.pose.pose.orientation
+        
+        rotm = quaternion_matrix([orien.x, orien.y, orien.z, orien.w])  # x, y, z, w;   quaternion is w + xi + yj + zk
+        rotm = np.array(rotm)
+        rotm = rotm[0:3, 0:3]
+
+        self.odom_pos = np.array([pos.y, pos.x, -pos.z])
+        self.odom_orien = rotm
 
     def cb_marker(self, marker):
         self.marker = np.array([marker.x, marker.y, marker.z])
 
+        # print("===marker====")
+        # print(self.marker)
+
+    def test_loop_duration(self, duration):
+        # continuously looping for a duration
+        starttime = time.time()
+        rate = rospy.Rate(10) 
+
+        while not rospy.is_shutdown():
+            curret = time.time()
+            sofar = curret - starttime
+            print(sofar)
+
+            if (sofar < duration) :
+                rate.sleep()
+            else:
+                break
 
 def main():
     rospy.init_node('talker', anonymous=True)
