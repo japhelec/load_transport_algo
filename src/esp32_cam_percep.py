37a495ca9bbcb833ad1e12c0ad8b1fef7a2c9b2b
KueiKao <r08921109@ntu.edu.tw>
2023-01-11 16:34:49 +0800

    esp32 camera aruco perception

---
+++ src/esp32_cam_percep.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+
+import sys
+import yaml
+import rospy
+import numpy as np
+import cv2
+from cv2 import aruco
+from cv_bridge import CvBridge
+from sensor_msgs.msg import Image
+from load_transport.msg import ap_position_msg, ap_rotation_msg
+
+#############################################
+#coordinate frame
+# c: camera frame
+# l: payload frame
+# m: captured marker frame
+# b: Q frame
+# P: ap on payload
+#############################################
+
+class Marker():
+    length = 0.05 #m
+
+class Perception():
+    def __init__(self):
+        # self.ap_id = int(rospy.get_param('~ap_id', 1))
+
+        with open('/home/kuei/catkin_ws/src/load_transport/src/camera_calib/esp32_intrinsic.yml', 'r') as f:
+            data = yaml.load(f, Loader=yaml.UnsafeLoader)
+        
+        self.mtx = np.array(data['mtx'])
+        self.dist = np.array(data['dist'])
+
+        self.br = CvBridge()
+        self.sub_image = rospy.Subscriber("/esp32/image_raw", Image, self.cb_marker_perception, queue_size = 1)
+        self.pub_ap_position = rospy.Publisher('/ap1/position', ap_position_msg, queue_size=1)
+        self.pub_ap_rotation = rospy.Publisher('/ap1/rotation', ap_rotation_msg, queue_size=1)
+
+    def cb_marker_perception(self, img_raw):
+        ## cvBridge
+        frame = self.br.imgmsg_to_cv2(img_raw)
+
+        ## detect
+        aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_250)
+        parameters =  aruco.DetectorParameters_create()
+        corners, ids, rejectedImgPoints = aruco.detectMarkers(frame,aruco_dict,parameters=parameters)
+        
+        ## estimation
+        if ids is not None:
+            # any marker is ok
+            id = ids[0][0]
+            corner = corners[0]
+            
+            rvec, tvec, _ = aruco.estimatePoseSingleMarkers(corner, Marker.length, self.mtx, self.dist)
+            
+            # print("=====================")
+            # print(tvec[0][0])
+            # R, jacob = cv2.Rodrigues(rvec[0][0])
+            # print(R)
+
+            msg = ap_position_msg()
+            msg.header.stamp = rospy.get_rostime()
+            msg.position = tvec[0][0]
+            self.pub_ap_position.publish(msg)
+
+            msg = ap_rotation_msg()
+            msg.header.stamp = rospy.get_rostime()
+            msg.rotation = rvec[0][0]
+            self.pub_ap_rotation.publish(msg)
+
+
+def main():
+    rospy.init_node('esp32_percep', anonymous=True)
+    Perception()
+    rospy.spin()
+
+
+if __name__ == '__main__':
+    main()
